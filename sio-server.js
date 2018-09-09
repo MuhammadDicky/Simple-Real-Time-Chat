c849532eda67c6ade06f073f81acd8e32ca0ae04
Muhammad Dicky Hidayat Latif <muh.dickyhidayat@gmail.com>
2018-09-10 02:14:50 +0800

    Change static path

--- sio-server.js
+++ sio-server.js
@@ -4,7 +4,7 @@ var app = express();
 var server = http.createServer(app).listen(3000);
 var io = require("socket.io")(server);
 
-app.use(express.static("./public"));
+app.use(express.static("http://app-c7e123d0-fe63-49b8-8047-eab30282183d.cleverapps.io/public"));
 
 io.on("connection", function (socket) {
     socket.on("chat", function (message) {
