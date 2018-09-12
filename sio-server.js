fe54b3ab476370e5e1bbed957c1f048abe1559b4
Muhammad Dicky Hidayat Latif <muh.dickyhidayat@gmail.com>
2018-09-13 01:48:55 +0800

    Adding timestamp for every chat

--- sio-server.js
+++ sio-server.js
@@ -8,9 +8,17 @@ app.use(express.static("./public"));
 
 io.on("connection", function (socket) {
     socket.on("chat", function (message) {
+        var timeStamp = Date(Date.now());
+        var message = {
+            message: message,
+            date: timeStamp
+        }
         socket.broadcast.emit("message", message);
     });
-    socket.emit("message", "Welcome to socket server");
+
+    socket.on("typing", function () {
+        socket.broadcast.emit("ontype", "On typing...");
+    });
 });
 
 console.log("Starting socket app on port 3000");
